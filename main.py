10cb60316661ab491fd5e79d8275f6ee7e9ca238
Jay Hemsley <9924527+jayhemsley@users.noreply.github.com>
2026-08-07 23:23:54 -0400

    Add Spam TLDs blocklist

--- main.py
+++ main.py
@@ -53,6 +53,7 @@ FOLDER_URLS = [
     "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/controld/nosafesearch-folder.json",
     "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/controld/referral-allow-folder.json",
     "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/controld/spam-idns-folder.json",
+    "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/controld/spam-tlds-folder.json",
     "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/controld/ultimate-known_issues-allow-folder.json",
 ]
 
